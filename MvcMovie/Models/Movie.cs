d2c5978a2e9aa21c319eb7a1cfdbecfbcba0cfec
Renato Netto <rnetto94@live.com>
2021-04-03 10:28:42 -0300

    primeiro commit

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        
+        [Display(Name = "Release Data")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+    }
+}
