4dcfa9d09eceec5b3a66be3529c9b3259debed1b
Renato Netto <rnetto94@live.com>
2021-04-03 11:52:34 -0300

    Adc campo de pesquisa por genero

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -20,16 +20,29 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
-        {   //pesquisa por nome
+        public async Task<IActionResult> Index(string searchString, string movieGenre)
+        {   // pesquisa de genero
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            //pesquisa por nome
             var movies = from m in _context.Movie
                          select m;
-            if (!String.IsNullOrEmpty(searchString))
+            if (!string.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+            var movieGenreVm = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
 
-            return View(await movies.ToListAsync());
+            return View(movieGenreVm);
         }
 
         // GET: Movies/Details/5
