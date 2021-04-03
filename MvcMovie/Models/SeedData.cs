d3c3e7dc81537e2062f7e310cf19561d380a50b7
Renato Netto <rnetto94@live.com>
2021-04-03 12:27:13 -0300

    Adc campo rating ao BD e nos views

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -23,7 +23,8 @@ namespace MvcMovie.Models
                      Title = "When Hary Met Sally",
                      ReleaseDate = DateTime.Parse("1989-2-12"),
                      Genre = "Romantic Comedy",
-                     Price = 7.99M
+                     Price = 7.99M,
+                     Rating = "R"
                  },
 
                 new Movie
@@ -31,21 +32,24 @@ namespace MvcMovie.Models
                     Title = "Ghostbusters",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
-                    Price = 8.99M
+                    Price = 8.99M,
+                    Rating = "R"
                 },
                 new Movie
                 {
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
-                    Price = 9.99M
+                    Price = 9.99M,
+                    Rating = "R"
                 },
                 new Movie
                 {
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
-                    Price = 3.99m
+                    Price = 3.99m,
+                    Rating = "R"
                 }
             );
             context.SaveChanges();
